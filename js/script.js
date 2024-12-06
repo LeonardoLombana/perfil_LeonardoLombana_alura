769c2eca2f388aea0fa27884aa85a9adb73136c4
LeonardoLombana <leonardo.lc02@hotmail.com>
2024-12-06 18:59:23 -0500

    Se crea footer y funcion de flecha hacia el inicio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-/*====== Sticky navbar ======*/
+/*====== Scroll sections active link======*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
 window.onscroll = () => {
+  sections.forEach(sec => {
+
+  });
+
+/*====== Sticky navbar ======*/  
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
