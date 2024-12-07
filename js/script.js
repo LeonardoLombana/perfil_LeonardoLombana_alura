f6cc51372eafa765c29f70c283aa498b8bf3d349
LeonardoLombana <leonardo.lc02@hotmail.com>
2024-12-06 23:17:55 -0500

    pagina finalizada en modo oscuro y animacion de textos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-/*====== Scroll sections active link======*/
+/*====== Menu icon navbar ======*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -21,6 +30,11 @@ window.onscroll = () => {
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
+
+/*====== remove menu icon navbar when click navbar link (scroll) ======*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+
 };
 
 /*====== Swiper ======*/
@@ -37,4 +51,26 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+});
+
+/*====== Dark light mode ======*/
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode')
+};
+
+
+/*====== Scroll reveal ======*/
+ScrollReveal({
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
